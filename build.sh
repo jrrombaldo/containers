4c3392bc7b3bbd7d545b4d3da772d6ef7be10ec7
Carlos Rombaldo <jr.rombaldo@gmail.com>
2019-06-08 09:40:45 +0100

    initial import

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export name=openvpn-as
+
+# building image
+docker build -t $name .
+
+# cleanning any container left behind
+docker ps -q --filter "name=$name" \
+    | grep -q . && docker rm -f $name
+
+# running it
+docker run -itd \
+    --name $name \
+    --rm \
+    --privileged \
+    -e EXTERNAL_HOST=localhost\
+    -p 9999:9999 \
+    -p 8888:8888 \
+    -p 8888:8888/udp \
+    $name:latest
+
+    # -v /tmp:/openvpnas_config \
+
+
+# https://openvpn.net/vpn-server-resources/advanced-option-settings-on-the-command-line/
+# https://openvpn.net/vpn-server-resources/managing-settings-for-the-web-services-from-the-command-line/

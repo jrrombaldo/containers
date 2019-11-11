f1614caa84da9566351b6f428d0f5f566a1423a5
Carlos Rombaldo Jr <jr.rombaldo@gmail.com>
2019-11-11 17:02:28 +0000

    fixing tautulli custom build

--- tautulli-rpi/build.sh
+++ tautulli-rpi/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # GENERIC IMAGE BUILD
-docker run --rm --privileged multiarch/qemu-user-static:register --reset
 git clone https://github.com/Tautulli/Tautulli-Docker
 cd ./Tautulli-Docker
 docker build -t $CONTAINER .
